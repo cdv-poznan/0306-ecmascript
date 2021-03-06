5744e3b194623a4dbd41792086c59cc37acd263d
Adrian Juszczak <adrian.juszczak@datawalk.com>
2021-03-06 13:04:12 +0100

    Methods

---
+++ scripts/methods.js
@@ -0,0 +1,48 @@
+/*
+console.log(Math.sign(-10));
+console.log(Math.sign(310));
+
+console.log(Math.trunc(10.9));
+
+console.log(Math.log10(100));
+
+console.log(Math.hypot(3, 4));
+
+console.log(Number.isInteger(1.23))
+console.log(Number.isInteger(23))
+console.log(Number.isInteger('asd'))
+
+
+console.log(Number.isNaN('asd'))
+console.log(isNaN('asd'))
+
+console.log(Number.isFinite(123));
+console.log(Number.isFinite(123 / 0));
+
+console.log('Hello!'.startsWith('Hel'));
+console.log('Hello!'.startsWith('hel'));
+
+console.log('Hello!'.endsWith('lo!'));
+console.log('Hello!'.endsWith('lo'));
+
+console.log('Hello!'.includes('ll'));
+
+const str1 = 'Hello!';
+
+console.log(str1.includes('el'));
+
+console.log(str1.repeat(3));
+
+*/
+
+const person1 = {
+  a: 1,
+  b: 2,
+  c: 4
+}
+
+console.log(Object.keys(person1));
+console.log(Object.values(person1));
+
+console.log(Object.keys([5,4,3]))
+
